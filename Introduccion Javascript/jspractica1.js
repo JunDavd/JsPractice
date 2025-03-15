dc8bd89c56555b13b67277df8c81a0a2465037bc
Juan Ruiz <jdruiz008@gmail.com>
2025-03-15 17:21:57 -0500

    add: japractica1

---
+++ Introduccion Javascript/jspractica1.js
@@ -0,0 +1,30 @@
+
+let numbers = [1,2,3,4,5]
+
+
+//numbers.forEach(display);
+numbers.forEach(double)
+
+
+//for each nos va a dar el elemento, el indice y el array en el que esta
+function double(element, index, array){
+   console.log(array[index] = element * 2);
+}
+
+function display(element) {
+    console.log(element)
+}
+
+let fruits = ['apple', 'orange', 'banana', 'coconut'];
+fruits.forEach(display)
+
+function display(element){
+    console.log(element)
+}
+
+
+
+
+
+
+
