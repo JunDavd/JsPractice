2e8154dfd17eda8413ac90eb69653f725116373b
Juan Ruiz <jdruiz008@gmail.com>
2025-03-15 20:30:25 -0500

    example: modify html with document.getElementById

--- practicaJs/index.js
+++ practicaJs/index.js
@@ -5,5 +5,14 @@
 // window.alert(`I like pizza`)
 
 
-document.getElementById('myH1').textContent = 'Hello'
-document.getElementById('myP').textContent = 'I like pizza'
+// document.getElementById('myH1').textContent = 'Hello'
+// document.getElementById('myP').textContent = 'I like pizza'}
+
+let fullName = 'Juan David'
+let age = 28
+let student = false
+
+document.getElementById('p1').textContent = `your name is ${fullName}`
+document.getElementById('p2').textContent = `you are ${age} years old`
+document.getElementById('p3').textContent = `enrolled: ${student}`
+
