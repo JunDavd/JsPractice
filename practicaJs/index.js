692d0ab8d0f46bc64d21ceb169ab47ec5d9e2c3f
Juan Ruiz <jdruiz008@gmail.com>
2025-03-15 19:06:51 -0500

    add: index.html index.js main.css

---
+++ practicaJs/index.js
@@ -0,0 +1,9 @@
+// console.log('Hello')
+// console.log('i like pizza')
+
+// window.alert(`this is an alert`)
+// window.alert(`I like pizza`)
+
+
+document.getElementById('myH1').textContent = 'Hello'
+document.getElementById('myP').textContent = 'I like pizza'
