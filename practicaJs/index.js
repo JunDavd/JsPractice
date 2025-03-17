d6c35a0155787014be835fa0b69ac5dd3c663981
Juan Ruiz <jdruiz008@gmail.com>
2025-03-17 10:52:01 -0500

    document manipulation with js

--- practicaJs/index.js
+++ practicaJs/index.js
@@ -8,11 +8,24 @@
 // document.getElementById('myH1').textContent = 'Hello'
 // document.getElementById('myP').textContent = 'I like pizza'}
 
-let fullName = 'Juan David'
-let age = 28
-let student = false
+// let fullName = 'Juan David'
+// let age = 28
+// let student = false
 
-document.getElementById('p1').textContent = `your name is ${fullName}`
-document.getElementById('p2').textContent = `you are ${age} years old`
-document.getElementById('p3').textContent = `enrolled: ${student}`
+// document.getElementById('p1').textContent = `your name is ${fullName}`
+// document.getElementById('p2').textContent = `you are ${age} years old`
+// document.getElementById('p3').textContent = `enrolled: ${student}`
 
+// let userName;
+
+// userName = window.prompt("what's your user name")
+
+// console.log(userName)
+
+// let userName;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     userName = document.getElementById('myText').value
+//     document.getElementById('myH1').textContent = `Hello ${userName}`
+    
+// }
