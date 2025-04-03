629c6b744e990d9fcab2d1e6441be93bf87f0f5c
Juan Ruiz <jdruiz008@gmail.com>
2025-04-03 08:23:58 -0500

    add counterProgram

--- practicaJs/index.js
+++ practicaJs/index.js
@@ -50,21 +50,3 @@
 //     let result = area(userValue)
 //     document.getElementById('myH3').textContent = result
 // }
-
-
-
-
-//COUNTER PROGRAM 
-
-const decreaseBtn = document.getElementById('decreaseBtn')
-const reserBtn = document.getElementById('resetBtn')
-const increaseBtn = document.getElementById('increaseBtn')
-const countLabel = document.getElementById('countLabel')
-
-
-let count = 0
-
-increaseBtn.onclick = function(){
-    count ++
-    countLabel.textContent = count
-}
